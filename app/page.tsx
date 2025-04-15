4e7c48392deb70ddd3305be54243aa98eb00233a
Griffin Westerman <gxweste@gmail.com>
2025-04-15 14:30:58 -0500

    feat: made a fun little music quiz app

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,129 @@
 "use client"
+import { motion, AnimatePresence } from "motion/react"
+import { Button } from "@/components/ui/button";
+import React, { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
+
+  // const notes = [
+  //   "C",
+  //   "C#",
+  //   "D",
+  //   "E♭",
+  //   "E",
+  //   "F",
+  //   "F#",
+  //   "G",
+  //   "A♭",
+  //   "A",
+  //   "B♭",
+  //   "B"
+  // ];
+
+  const notes = [
+    "G",
+    "F",
+    "E",
+    "D",
+    "C",
+    "B",
+    "A",
+    "G",
+    "F",
+    "E",
+    "D",
+  ];
+
+  const answers = [
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "A",
+    "B"
+  ];
+
+  const positions = [
+    -90,
+    -72,
+    -54,
+    -36,
+    -18,
+    0,
+    18,
+    36,
+    54,
+    72,
+    90
+  ];
+
+  const [step, setStep] = useState(0);
+  const [visible, setVisible] = useState(false);
+  const [answer, setAnswer] = useState('');
+  const [score, setScore] = useState(0);
+
+  function getNextStep(currentStep: number) {
+    let newStep;
+    do {
+      newStep = Math.floor((Math.random() * 11) % 11);
+    } while (newStep === currentStep);
+    return newStep;
+  }
+
+  function goNext(e: React.MouseEvent<HTMLButtonElement>) {
+    if (e.currentTarget.value === notes[step]) {
+      setAnswer("correct");
+    } else {
+      setAnswer("wrong");
+    }
+    setVisible(false);
+    setStep(getNextStep(step));
+  }
+
+  useEffect(() => {
+    if (!visible) {
+      const timer = setTimeout(() => {
+        setAnswer("");
+        setVisible(true);
+      }, 750);
+      return () => clearTimeout(timer);
+    }
+  }, [visible]);
  
   return (
-    <main className="w-screen h-screen flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <div className="grid gap-10">
-        <div className="relative">
-          <div className="w-60 h-[1px] bg-gradient-to-r from-white/10 via-white/50 to-white/10 from-1% via-50% to-99%" />
-          <div className="absolute h-4 w-4 bg-white right-28 rounded-full top-[-0.45rem]" />
-        </div>
-        <div className="relative">
-          <div className="w-60 h-[1px] bg-gradient-to-r from-white/10 via-white/50 to-white/10 from-1% via-50% to-99%" />
-          <div className="absolute h-4 w-4 bg-white right-28 rounded-full top-[-0.45rem]" />
-        </div>
-        <div className="relative">
-          <div className="w-60 h-[1px] bg-gradient-to-r from-white/10 via-white/50 to-white/10 from-1% via-50% to-99%" />
-          <div className="absolute h-4 w-4 bg-white right-28 rounded-full top-[-0.45rem]" />
-        </div>
-        <div className="relative">
-          <div className="w-60 h-[1px] bg-gradient-to-r from-white/10 via-white/50 to-white/10 from-1% via-50% to-99%" />
-          <div className="absolute h-4 w-4 bg-white right-28 rounded-full top-[-0.45rem]" />
-        </div>
-        <div className="relative">
-          <div className="w-60 h-[1px] bg-gradient-to-r from-white/10 via-white/50 to-white/10 from-1% via-50% to-99%" />
-          <div className="absolute h-4 w-4 bg-white right-28 rounded-full top-[-0.45rem]" />
-        </div>
+    <main className={cn(answer === "correct" ? "bg-green-100" : answer === "wrong" ? "bg-red-100" : "bg-blue-50", "transition-[background-color] duration w-screen h-screen flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]")}>
+      <div className="flex flex-col justify-center gap-8 relative overflow-hidden w-70 h-60">
+        <div>{score}</div>
+        <AnimatePresence initial={false}>
+          {visible ?
+            <motion.div
+              className="z-10 absolute h-6 w-6 right-33 bg-blue-700 rounded-full"
+              initial={{ x: 400, y: positions[step] }}
+              animate={{ x: 0 }}
+              exit={{ x: -400 }}
+              transition={{ type: "spring", duration: 0.5, delay: (step % 5) * 0.05 }}
+            /> : null
+          }
+        </AnimatePresence>
+        {Array(5).fill(0).map((_, index) => (
+          <AnimatePresence key={`${index}`} initial={false}>
+            {visible ?
+              <motion.div
+                className="w-full h-1 rounded-full bg-blue-300"
+                initial={{ x: 400 }}
+                animate={{ x: 0 }}
+                exit={{ x: -400 }}
+                transition={{ type: "spring", duration: 0.5, delay: index * 0.05 }}
+              /> : null}
+            </AnimatePresence>
+        ))}
+      </div>
+      <div className="flex gap-1">
+        {answers.map((answer, index) => (
+          <Button value={answer} className="hover:cursor-pointer text-blue-400 hover:bg-blue-400/50 hover:text-blue-100" onClick={(e) => goNext(e)} key={index} size="icon" variant="ghost">{answer}</Button>
+        ))}
       </div>
     </main>
   );
